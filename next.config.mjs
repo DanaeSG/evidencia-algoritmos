b09cd4d7f0a85d79f6dacd31fa27a008c3f82e38
Danaé Sánchez <117100245+DanaeSG@users.noreply.github.com>
2024-10-18 21:06:46 -0600

    hi

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
-// next.config.mjs
-const nextConfig = {
-    output: 'export',
-    basePath: '/evidencia-algoritmos', // Reemplaza con el nombre de tu repositorio
-  };
-  
-  export default nextConfig;
-  
+const nextConfig = {};
+
+export default nextConfig;
