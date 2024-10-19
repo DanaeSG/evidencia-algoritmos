5c41b804bdd981562154ee11ab27ddd24e8de2d4
Danaé Sánchez <117100245+DanaeSG@users.noreply.github.com>
2024-10-18 20:39:55 -0600

    publicar

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+// next.config.mjs
+const nextConfig = {
+    output: 'export',
+    basePath: '/evidencia-algoritmos', // Reemplaza con el nombre de tu repositorio
+  };
+  
+  export default nextConfig;
+  
